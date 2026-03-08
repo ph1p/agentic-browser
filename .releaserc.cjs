b60eece72f98f77bffc2177357d2d49df3b88029
ph1p <me@ph1p.dev>
2026-03-08 18:34:59 +0100

    refactor: update semantic release config

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
   branches: ["main"],
+  releaseRules: [
+    { breaking: true, release: "major" },
+    { type: "feat", release: "minor" },
+    { type: "fix", release: "patch" },
+    { type: "perf", release: "patch" },
+    { type: "revert", release: "patch" },
+  ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
